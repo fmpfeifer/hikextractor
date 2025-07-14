3e48c9164e049cc7fa28c0cbb0d5a5f028212761
Catalin Patulea <cronos586@gmail.com>
2025-07-13 23:27:27 -0400

    Allow running directly on a block device.

--- src/hikextractor.py
+++ src/hikextractor.py
@@ -325,8 +325,14 @@ def rename_file_if_exists(filename: str) -> str:
     return new_filename
 
 
+def get_file_size(fp):
+    return fp.seek(0, os.SEEK_END)
+
+
 def export_all_videos(source, dest_folder, list_only=False, master_only=False, raw=False):
-    with open(source, "rb") as input_image, mmap.mmap(input_image.fileno(), 0, access=mmap.ACCESS_READ) as mmapped_file:
+    with open(source, "rb") as input_image, mmap.mmap(
+        input_image.fileno(), get_file_size(input_image), access=mmap.ACCESS_READ
+    ) as mmapped_file:
         try:
             master = parse_master_block(mmapped_file)
             print(f"HD Signature: {master.signature.decode('utf-8')}")
@@ -443,7 +449,7 @@ if __name__ == "__main__":
 
     source = args.input
     dest_folder = args.output
-    if not os.path.isfile(source):
+    if not os.path.exists(source):
         print(f"File not found: {source}", file=sys.stderr)
         exit(1)
     if args.master_only:
